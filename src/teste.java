0dece90a38beee3ee4cd9d1d4e0da8a2ff447abb
Ana Paula Souza Silva <apss7@discente.ifpe.edu.br>
2022-08-12 20:32:31 -0300

    segunda mudança

--- src/teste.java
+++ src/teste.java
@@ -3,5 +3,6 @@ public class teste {
 	public static void main(String[] args) {
 		System.out.println("testando alterações!");
 		System.out.println("mudança 1 ");
+		System.out.println("deu tudo certo uhuuu ");
 	}
 }
